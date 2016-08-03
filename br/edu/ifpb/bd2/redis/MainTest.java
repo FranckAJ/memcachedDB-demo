6b94b132a6b3b8950b8b8b87a0cbdb3c2a1a29aa
JoseRafael97 <jose.rafael.feitosa@gmail.com>
2016-08-02 22:45:59 -0300

    mudando para redis

---
+++ br/edu/ifpb/bd2/redis/MainTest.java
@@ -0,0 +1,20 @@
+package edu.ifpb.bd2.redis;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+public class MainTest {
+	
+	public static void main(String[] args) {
+		Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
+		//Jedis Cluster will attempt to discover cluster nodes automatically
+		jedisClusterNodes.add(new HostAndPort("127.0.0.1", 7379));
+		JedisCluster jc = new JedisCluster(jedisClusterNodes);
+		jc.set("foo", "bar");
+		String value = jc.get("foo");
+	}
+
+}
